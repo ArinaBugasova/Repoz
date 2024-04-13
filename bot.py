393e5028acdf89b22c049b0bdebf02022d71a1f9
Арина <email@example.comarinabugasova@gmail.com>
2024-04-13 12:26:16 +0300

    222

---
+++ bot.py
@@ -0,0 +1,59 @@
+import telebot
+from config import token
+
+API_TOKEN = token
+
+bot = telebot.TeleBot(API_TOKEN)
+
+f = 0
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.reply_to(message, """\\""")
+
+
+@bot.message_handler(commands=['info'])
+def send_welcome(message):
+    bot.reply_to(message, """\
+        выберете цвет командой /color
+выберете номер командой /number""")
+    f = 0
+
+@bot.message_handler(commands=['color'])
+def send_welcome3(message):
+    global f
+    global color
+    bot.reply_to(message, "ваш цвет?")
+    f = 1
+
+@bot.message_handler(commands=['number'])
+def send_welcome4(message):
+    global f
+    global n
+    bot.reply_to(message, "ваш номер?")
+    f = 2
+
+
+@bot.message_handler(func=lambda message: True)
+def val(message):
+    global n
+    global color
+    if f == 1:
+        color = message.text
+        bot.reply_to(message, 'ваш цвет: ' + message.text)
+    if f == 2:
+        n = message.text
+        bot.reply_to(message, 'ваш номер: ' + message.text)
+    
+@bot.message_handler(content_types=['new_chat_members'])
+def make_some(message):
+    bot.send_message(message.chat.id, 'новый челик')
+    bot.approve_chat_join_request(message.chat.id, message.from_user.id)
+
+# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
+#@bot.message_handler(func=lambda message: True)
+#def echo_message(message):
+ #   bot.reply_to(message, message.text)
+
+
+bot.infinity_polling()
